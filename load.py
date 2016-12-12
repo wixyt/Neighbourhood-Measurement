754518a53402445e23f7d4f41a6228696f9858e0
wixt <tim.wicks.9001@gmail.com>
2016-12-12 15:46:00 +1100

    added more tests

--- load.py
+++ load.py
@@ -4,54 +4,76 @@ import snap
 import networkx as nx
 import numpy as np
 import os, os.path
-import matplotlib.pyplot as plt
 import glob
 import math
 import operator
 from sklearn.feature_extraction import DictVectorizer
-from networkx.readwrite import json_graph
-import json
+
+
 
 
 class GraphLoader(object):
-    def __init__(self, args):
-        self.directed = args[1]
-        # TODO: add elif for multigraphs
-        if self.directed:
-            print ("Returning Directed Graph...")
-            self.graph = nx.DiGraph()
-        else:
-            print ("Returning Undirected Graph...")
-            self.graph = nx.Graph()
-        self.path = os.path.join(os.path.dirname(os.path.realpath(__file__)) + "/" + args[0])
-        self.file_nodes = [int(x.split("/")[-1].split('.')[0]) for x in glob.iglob(self.path + "/*.featnames")]
+    def __init__(self):
+        self.directed = bool
+        self.path = str
+        self.file_nodes = [ int ] 
 
     
     def save(self, path):
+        try: 
+            from networkx.readwrite import json_graph
+        except ImportError as e:
+            print "Import error loading networkx json_graph:\n %s" % e
+            return False
+        try:
+            import json
+        except ImportError as e:
+            print "Json import error: %s" % e
+            return False
+        
         save_file = json_graph.node_link_data(self.graph)
         json.dump(save_file, open(path, 'w'), indent=2)
+        return True
 
-    def load_files(self):
+    def load_files(self, args):
+        self.path = os.path.join(os.path.dirname(os.path.realpath(__file__)) + "/" + args[0])
+        self.file_nodes = [int(x.split("/")[-1].split('.')[0]) for x in glob.iglob(self.path + "/*.featnames")]
+        if self.directed:
+            print ("Directed Graph...")
+            self.graph = nx.DiGraph()
+        else:
+            print ("Undirected Graph...")
+            self.graph = nx.Graph()
+
         subgraph_count = 0 # value to keep track on total number of subgraphs
-        for node in self.file_nodes: 
-            # get file paths
-            feature_name_file = os.path.join(self.path + "/%s.featnames" % node)
-            edge_file = os.path.join(self.path + "/%s.edges" % node)
-            edge_feat_file = os.path.join(self.path + "/%s.feat" % node)
-            node_feat_file = os.path.join(self.path + "/%s.egofeat" % node)
-            circles_file = os.path.join(self.path + "/%s.circles" % node)
-            
-            # load nodes/attributes onto graph
-            
-            self.graph.add_node(node)
-            attribute_dict = self.load_attributes(feature_name_file)
+        try:
+            for node in self.file_nodes: 
+                try:
+                    # establish file paths
+                    feature_name_file = os.path.join(self.path + "/%s.featnames" % node)
+                    edge_file = os.path.join(self.path + "/%s.edges" % node)
+                    edge_feat_file = os.path.join(self.path + "/%s.feat" % node)
+                    node_feat_file = os.path.join(self.path + "/%s.egofeat" % node)
+                    circles_file = os.path.join(self.path + "/%s.circles" % node)
+                except Exception as e:
+                    print "Error loading file path: %s" % e
+                    return False
+                # load nodes/attributes onto graph
+                
+                    self.graph.add_node(node)
+                    attribute_dict = self.load_attributes(feature_name_file)
 
-            self.load_node_features(node_feat_file, attribute_dict, node)
-            self.load_nodes(edge_feat_file, attribute_dict)
-            self.load_edges(edge_file)
-            self.add_empty_subgraph()
-            subgraph_count += self.apply_ground_truths(circles_file, subgraph_count)
-        self.named_attributes_to_vector()
+                    self.load_node_features(node_feat_file, attribute_dict, node)
+                    self.load_nodes(edge_feat_file, attribute_dict)
+                    self.load_edges(edge_file)
+                    self.add_empty_subgraph()
+                    subgraph_count += self.apply_ground_truths(circles_file, subgraph_count)
+                
+            self.named_attributes_to_vector()
+        except IOError:
+            print "IOError in main load function"
+            return False
+        return True
 
     def load_attributes(self, feature_file_path):
         # create dictionary of features keys and values
@@ -70,11 +92,16 @@ class GraphLoader(object):
 
     def load_node_features(self, node_feat_file, feature_index, node_id):
         key_array = feature_index.keys()
-        with open(node_feat_file, 'r') as file:
-            vector_string = file.read()[:-1]
-            node_feat_vect = [int(x) for x in vector_string.split(' ')]
-            self.apply_attribute_vector(node_feat_vect, node_id, feature_index)
-            self.apply_attributes(node_feat_vect, node_id, feature_index)
+        try:
+            file = open(node_feat_file)
+        except IOError:
+            print "Could not open node feature file %s" % node_feat_file
+            return False
+        vector_string = file.read()[:-1]
+        node_feat_vect = [int(x) for x in vector_string.split(' ')]
+        self.apply_attribute_vector(node_feat_vect, node_id, feature_index)
+        self.apply_attributes(node_feat_vect, node_id, feature_index)
+        return True
 
     
     def apply_attribute_vector(self, feature_vector, node_id, feature_dict):
@@ -90,29 +117,38 @@ class GraphLoader(object):
         for key in range(len(feature_dict)):
             self.graph.node[node_id]['named_attributes'][feature_dict[str(key)]] = feature_vector[int(key)]
 
+
     def load_nodes(self, edge_feat_path, feature_index):
         key_array = feature_index.keys()
-        with open(edge_feat_path, 'r') as file:
-            for line in file:
-                try:
-                    features = [int(x) for x in line.split(' ')]
-                    node = features[0]
-                    feature_vect = features[1:]
-                    self.graph.add_node(node)
-                    
-                    self.apply_attribute_vector(feature_vect, node, feature_index)
-                    self.apply_attributes(feature_vect, node, feature_index)
-                except Exception as e:
-                    print "error: %s" % e
+        try:
+            file = open(edge_feat_path)
+        except IOError:
+            print "Error reading edge feature file"
+            return False
+        for line in file:
+            try:
+                features = [int(x) for x in line.split(' ')]
+                node = features[0]
+                feature_vect = features[1:]
+                self.graph.add_node(node)
+                
+                self.apply_attribute_vector(feature_vect, node, feature_index)
+                self.apply_attributes(feature_vect, node, feature_index)
+            except Exception as e:
+                print "Error at line %s: %s" % (line, e)
+        return True
+
 
     def load_edges(self, path):
         try:
             edge_file_contents = open(path)
         except IOError:
             print "Error reading edge file contents"
+            return False
         for line in edge_file_contents:
             edges = [int(x) for x in line.split(' ')]
             self.graph.add_edge(edges[0], edges[1])
+        return True
 
     
     def add_empty_subgraph(self):
@@ -147,13 +183,18 @@ class GraphLoader(object):
         v = DictVectorizer(sparse=False)
         for node in self.graph.nodes():
             X = v.fit_transform([total, self.graph.node[node]['named_attributes']])
-            # nx.set_node_attributes(G, 'full_featvector', {node: X[1].tolist()})
             self.graph.node[node]['feature_vector'] = X[1].tolist() # numpy arrays cannot be stored properly
         
 
     def draw_nx_graph(self):
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            print "Matplotlib import error"
+            return False
         nx.draw(self.graph)
         plt.show()
+        return True
 
 
 def edge_list_from_file(path, delim="\t"):
--- main.py
+++ main.py
@@ -11,6 +11,85 @@ def parse_arguments():
 
     return args
 
+class TestingError(Exception):
+    """
+        Error raised if testing fails
+    """
+
+def test_load_module(module):
+    """
+        Test that module contains the appropriate elements
+    """
+    print "-" * 40
+    print "Testing graph loader module"
+    print "-" * 40
+    try:
+        gl_class = module.GraphLoader
+        if type(gl_class) != type:
+            print "GraphLoader is not a class"
+            class tc: pass
+            if type(gl_class) == type(tc):
+                print "GraphLoader does not inherit from Object Class"
+            raise TestingError()
+    except AttributeError:
+        print "Graph loader does not exist"
+        raise TestingError()
+    
+    try:
+        graph_loader = module.GraphLoader()
+    except Exception as e:
+        print "Error calling Graph Loader: %s" % e
+        raise TestingError()
+
+    dummy_test = '/test/file/location/'
+    try:
+        rsf = graph_loader.load_files
+        print "-" * 40
+        print "Testing load file method..."
+        print "-" * 40
+        try:
+            if rsf(dummy_test):
+                print "Inadvertently returned true from loading dummy file"
+                raise TestingError()
+        except IOError:
+            print "Uncaught IOError"
+            raise TestingError()
+        except Exception as e:
+            print "Unexpected error: %s" % e
+            raise TestingError()
+        
+        try:
+            if hasattr(module.GraphLoader, 'path'):
+                print "'path' is a class variable and does not belong to the instance"
+                raise TestingError()
+
+            if hasattr(module.GraphLoader, 'directed'):
+                print "'directed' is class variable and does not belong to the instance"
+                raise TestingError()
+
+            if hasattr(module.GraphLoader, 'graph'):
+                print "'graph' is a class variable and does not belong to the instance"
+                raise TestingError()
+
+            if hasattr(module.GraphLoader, 'file_nodes'):
+                print "'file_nodes' is a class variable and does not belong to the instance"
+                raise TestingError()
+        except TestingError:
+            raise TestingError()
+        except Exception as e:
+            print "unhandled exception: %s" % e
+            raise TestingError()
+    except AttributeError:
+        print "load files does not exist!"
+        raise TestingError()
+    
+    test_load = module.GraphLoader()
+    test_load.load_files('testdata/', False)
+    print type(test_load.graph)
+    assert test_load.graph
+
+
+
 def main():
 
     args = parse_arguments()
@@ -20,9 +99,15 @@ def main():
     except ImportError:
         print("load.py file does not exist")
         sys.exit()
+    
+    if args.data:
+        try:
+            test_load_module(load)
+        except TestingError:
+            pass
 
-    graphObj = load.GraphLoader([args.data, args.bigraph])
-    graphObj.load_files()
+    graphObj = load.GraphLoader()
+    graphObj.load_files([args.data, args.bigraph])
     
     print "Size of graph: %d" % graphObj.graph.size()
     print "Number of nodes: %d" % len(graphObj.graph.nodes())
